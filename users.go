53cc89f00076529f114f1d6f47b58e947a9aea0f
duke1swd <swd@pobox.com>
2022-01-16 10:55:58 -0500

    Minor cleanup of users.go

--- users.go
+++ users.go
@@ -28,31 +28,29 @@ func usersCmd() {
 	// open database
 	db, err := sql.Open("postgres", psqlconn)
 	CheckError(err)
-
-	// close database
 	defer db.Close()
 
-	// check db
+	// check db is accessible
 	err = db.Ping()
 	CheckError(err)
 
-	fmt.Println("Connected!")
-
-	// select email, listsrv, wwwsrv, burpsrv from email;
+	// Get the email addresses
 	rows, err := db.Query("select email, listsrv, wwwsrv, burpsrv from email")
 	CheckError(err)
 	defer rows.Close()
 
 	for rows.Next() {
 		var (
-			email string
+			email   string
 			listsrv bool
-			wwwsrv bool
+			wwwsrv  bool
 			burpsrv bool
 		)
 
 		err = rows.Scan(&email, &listsrv, &wwwsrv, &burpsrv)
 		CheckError(err)
+
+		// Print in simple CSV format
 		fmt.Printf("%s,%v,%v,%v\n", email, listsrv, wwwsrv, burpsrv)
 	}
 }
